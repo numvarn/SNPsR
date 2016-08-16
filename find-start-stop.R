2475ca8fa1fa66dc392fffc71474899980eeadc2
numvarn <phisan.shukkhi@gmail.com>
2016-08-16 13:16:43 +0700

    modified:   find-start-stop.R

--- find-start-stop.R
+++ find-start-stop.R
@@ -10,37 +10,26 @@ outfile_start_stop <- "result/StartStopResult.csv"
 gene_pair_data <- read.csv(gene_pair_path, header = TRUE)
 number_row <- nrow(gene_pair_data)
 
-current_gene <- ""
-start_gene <- ""
-start_point <- 0
-stop_point <- 0
+start_point <- 1
+stop_point <- 1
 start_stop_vector <- c()
+gene_number <- 0
 
 for (i in 1:number_row) {
-     switch_flag = FALSE
-     current_gene = as.character(gene_pair_data[i, 3])
-     if (start_gene != current_gene) {
-          # found stop point
-          stop_point <- i
-          switch_flag = TRUE
-     } else if (start_gene == current_gene) {
-          stop_point <- i
-     } 
+     current_gene <- as.character(gene_pair_data[i, 3])
+     next_gene <- as.character(gene_pair_data[(i+1), 3])
      
-     if (switch_flag == TRUE) {
-          start_stop_vector <- append(start_stop_vector, c(start_gene, start_point, stop_point))
-          start_point <- i
-          start_gene <- current_gene
-     }
-     
-     if (i < 50) {
-          cat(sprintf("%s : %s : %s, %s\n", i, as.character(gene_pair_data[i, 3]), start_point, stop_point)) 
+     if (current_gene != next_gene && i < number_row) {
+          gene_number <- gene_number + 1
+          stop_point <- i
+          start_stop_vector <- append(start_stop_vector, c(gene_number, as.character(gene_pair_data[i, 3]), start_point, stop_point))
+          start_point <- i + 1
      }
 }
 
-start_stop_matrix <- matrix(start_stop_vector, length(start_stop_vector)/3, 3, byrow = TRUE)
+start_stop_matrix <- matrix(start_stop_vector, length(start_stop_vector)/4, 4, byrow = TRUE)
 
-colnames(start_stop_matrix) <- c("Gene", "Start", "Stop")
+colnames(start_stop_matrix) <- c("Gene No.", "Gene Name", "Start", "Stop")
 write.csv(start_stop_matrix, file = outfile_start_stop, row.names = FALSE)
 
 
