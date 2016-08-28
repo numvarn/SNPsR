5174c5f03233cf976b82ec84f5ef889382fcc545
numvarn <phisan.shukkhi@gmail.com>
2016-08-28 12:02:23 +0700

    renamed:    gene-prepairing/bash.sh -> gene-prepairing/bash-gen-snps.sh 	new file:   misc/change-file-name.R 	renamed:    check-snps/check-snps.R -> misc/check-snps.R

--- gene-prepairing/bash-gen-snps.sh
+++ gene-prepairing/bash-gen-snps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for var in {1..500}
+for var in {1..1000}
 do
   echo $var
   /usr/local/bin/Rscript /Users/phisan/ResearchCode/SNPsR/R/gene-prepairing/gen-snps.R
---
+++ misc/change-file-name.R
@@ -0,0 +1,18 @@
+# Config values
+setwd("/Volumes/Phisan Segate/case-control-simulate")
+
+
+filez <- list.files("./")
+i <- 1
+for (name in filez) {
+     if (i < 10) {
+          prefix <- "000"
+     }else if (i < 100) {
+          prefix <- "00"
+     }else if (i <= 1000) {
+          prefix <- ""
+     } 
+     file.rename(name, paste(prefix, i, ".csv", sep = ""))
+     cat(sprintf("%s %s\n", i, name))
+     i <- i + 1
+}
